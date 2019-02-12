5d132debee029d758f9e2fe83b035f4deaa03f75
kakeru.watanabe <jueda1104@gmail.com>
2019-02-12 08:08:05 +0000

    like-2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,13 @@ class LikesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.fav(micropost)
     flash[:success] = 'お気に入りに追加しました。'
-    redirect_to current_user
+    redirect_back(fallback_location: root_url)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfav(micropost)
     flash[:success] = 'お気に入りから削除しました。'
-    redirect_to current_user
+    redirect_back(fallback_location: root_url)
   end
 end
